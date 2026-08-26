93276e2b6649dcf3de8052212f3982415f0ae906
eduardosilva0203540-prog <eduardosilva0203540@gmail.com>
2026-08-26 08:20:18 -0300

    cria scripts de precipitacao e vento

---
+++ scripts/precipitacao.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD507yI1k5nIKAHit-xNEglMfbvyU-mwjo",
+  databaseURL: "https://lecitech-78671-default-rtdb.firebaseio.com/",
+  projectId: "lecitech-78671"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const chuvaRef = ref(database, 'estacao/chuva');
+const displayVal = document.getElementById('valor-atual-destaque');
+
+onValue(chuvaRef, (snapshot) => {
+  const val = snapshot.val();
+  if (displayVal) displayVal.innerText = val !== null ? val : '--';
+});
